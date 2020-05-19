38181994d38453f154bcf6ef58c1cd265a3e2b48
Danielle Spencer <d.m.spencer.0486@gmail.com>
2020-05-19 16:09:19 -0500

    added createPost action and updated form and post components

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,2 +1,2 @@
-export const FETCH_POSTS = 'FETH_POSTS';
+export const FETCH_POSTS = 'FETCH_POSTS';
 export const NEW_POST = 'NEW_POST'
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -8,11 +8,17 @@ class Posts extends Component {
     componentDidMount(){
         this.props.fetchPosts();
     }
+
+    componentWillReceiveProps(nextProps){
+        if(nextProps.newPost){
+            this.props.posts.unshift(nextProps.newPost)
+        }
+    }
     
     render() {
         const postItems = this.props.posts.map(post => (
             <div key={post.id}>
-                <h3>{}post.title</h3>
+                <h3>{post.title}</h3>
                 <p>{post.body}</p>
             </div>
         ));
@@ -29,10 +35,12 @@ class Posts extends Component {
 Posts.propTypes = {
     fetchPosts: PropTypes.func.isRequired,
     posts: PropTypes.array.isRequired,
+    newPost: PropTypes.object,
 }
 
 const mapStateToProps = state => ({
-    posts: state.posts.items
+    posts: state.posts.items,
+    newPost: state.posts.item
 })
 
 export default connect(mapStateToProps, { fetchPosts })(Posts)
