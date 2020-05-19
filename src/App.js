7bede15a044c161491fb9290274f363736f8a3c6
Danielle Spencer <d.m.spencer.0486@gmail.com>
2020-05-19 14:50:20 -0500

    added postform component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
-import Posts from './components/Posts'
+import Posts from './components/Posts';
+import PostForm from './components/PostForm'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <PostForm />
+      <hr />
       <Posts />
     </div>
   );
